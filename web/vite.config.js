5d59bdca77e9f55af7c6e3e3777c5ac22425b3b9
sankalpa8586 <sankalpa8586@gmail.com>
2026-04-22 11:31:55 -0400

    Fix output directory for Vercel

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  build: {
+    outDir: 'dist', // Set the output directory to "dist"
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
